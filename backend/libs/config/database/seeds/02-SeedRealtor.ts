22ce0db8ea52f655d0d888553933011586911b64
Jacob Goh <jacob.goh@carsome.com>
2021-07-18 21:26:34 +0800

    fix(seeds): fix realtor seeders

--- backend/libs/config/database/seeds/02-SeedRealtor.ts
+++ backend/libs/config/database/seeds/02-SeedRealtor.ts
@@ -8,27 +8,32 @@ export async function seed(knex: Knex): Promise<any> {
   const userModel = () => knex<UserModel>(UserModel.tableName);
   const casbinRuleModel = () => knex<CasbinRule>(CasbinRule.tableName);
   await addRealtor(
+    2,
     'jacobgoh101+seeded-realtor-1@gmail.com',
     'Seeded Realtor 1',
   );
   await addRealtor(
+    3,
     'jacobgoh101+seeded-realtor-2@gmail.com',
     'Seeded Realtor 2',
   );
 
-  async function addRealtor(email: string, name: string) {
-    await userModel()
-      .insert({
-        email,
-        passwordHash: BCRYPT.hashPasswordSync('Pa$$w0rd!'),
-        name,
-        emailVerified: true,
-        blocked: false,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })
-      .catch(() => null);
-    const user = await userModel().where('email', email).first();
+  async function addRealtor(id: number, email: string, name: string) {
+    let user = await userModel().where('email', email).first();
+    if (!user)
+      try {
+        await userModel().insert({
+          id,
+          email,
+          passwordHash: BCRYPT.hashPasswordSync('Pa$$w0rd!'),
+          name,
+          emailVerified: true,
+          blocked: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+        user = await userModel().where('id', id).first();
+      } catch (error) {}
     const realtorCasbinQuery = {
       ptype: 'g',
       v0: `${user.id}`,
